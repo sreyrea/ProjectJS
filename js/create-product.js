67a75245e893e0aa6cee0836fc2278c6f8ed23eb
sreyrea <hansreyrea2222@gmail.com>
2022-11-29 20:59:44 +0700

    [CREATE_PRODUCT]: create dialog in file html create-product

---
+++ js/create-product.js
@@ -0,0 +1,9 @@
+
+
+function product(){
+    const cancel = document.querySelector("#create");
+    const name_prosuct = document.querySelector("#name");
+    console.log(name_prosuct);
+}
+ElementInternals("click", cancel);
+
